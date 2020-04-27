4ab2238bfabe37bd32a1cb428dae2e940c4916ad
Kyaw Aung Win <kyawagwin@gmail.com>
2020-04-27 20:19:39 +0800

    setup router

---
+++ src/router.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./container/Home";
+import Login from "./container/Login";
+
+const routes = [
+  {
+    path: "/",
+    component: Home,
+    exact: true,
+  },
+  { path: "/login", component: Login },
+];
+
+const Routes = () => {
+  return (
+    <Switch>
+      {routes.map(({ path, component, exact = false }) => (
+        <Route path={path} component={component} exact={exact} />
+      ))}
+    </Switch>
+  );
+};
+
+export default Routes;
