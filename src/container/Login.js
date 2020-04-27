9e203f6a9dfab1f52133121e3e43185ad30e813f
Kyaw Aung Win <kyawagwin@gmail.com>
2020-04-27 21:39:52 +0800

    added login form

--- src/container/Login.js
+++ src/container/Login.js
@@ -1,7 +1,85 @@
 import React from "react";
+import axios from "axios";
+import { Form, Input, Button, message } from "antd";
+
+const layout = {
+  labelCol: {
+    span: 2,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 2,
+    span: 16,
+  },
+};
 
 const Login = () => {
-  return <h1>Login</h1>;
+  const onFinish = ({ email, password }) => {
+    axios
+      .post("https://reqres.in/api/login", { email, password })
+      .then((response) => {
+        console.log(response, "response");
+        const { token } = response.data;
+      })
+      .catch((error) => {
+        console.log(error, "error");
+        message.error("User not found");
+      });
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    message.info("Provide email and password");
+  };
+
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <h1>Log in</h1>
+
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: "Please input your email address!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default Login;
--- src/container/SiteLayout.js
+++ src/container/SiteLayout.js
@@ -1,27 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { Layout, Menu } from "antd";
+
+import { AuthContext } from "../context/AuthProvider";
 
 import "./SiteLayout.css";
 
 const { Header, Footer, Content } = Layout;
 
-const SiteLayout = ({ children }) => (
-  <Layout>
-    <Header>
-      <div className="logo">EzySign</div>
-      <Menu mode="horizontal" theme="dark" defaultSelectedKeys={["0"]}>
-        <Menu.Item key="0">
-          <NavLink to="/">Home</NavLink>
-        </Menu.Item>
-        <Menu.Item key="1">
-          <NavLink to="/login">Log in</NavLink>
-        </Menu.Item>
-      </Menu>
-    </Header>
-    <Content className="content">{children}</Content>
-    <Footer>&copy;EzySign</Footer>
-  </Layout>
-);
+const SiteLayout = ({ children }) => {
+  const { loggedIn } = useContext(AuthContext);
+
+  return (
+    <Layout>
+      <Header>
+        <div className="logo">EzySign</div>
+        <Menu mode="horizontal" theme="dark" defaultSelectedKeys={["0"]}>
+          <Menu.Item key="0">
+            <NavLink to="/">Home</NavLink>
+          </Menu.Item>
+          {loggedIn ? (
+            <Menu.Item key="2">
+              <NavLink to="/logout">Log out</NavLink>
+            </Menu.Item>
+          ) : (
+            <Menu.Item key="1">
+              <NavLink to="/login">Log in</NavLink>
+            </Menu.Item>
+          )}
+        </Menu>
+      </Header>
+      <Content className="content">{children}</Content>
+      <Footer>&copy;EzySign</Footer>
+    </Layout>
+  );
+};
 
 export default SiteLayout;
